62aad3fcd78dbaebe1437616967198e4109a99ec
kierkat10 <kiernan.simpson@gmail.com>
2025-11-07 16:25:30 -0700

    format stuff and add greek folder

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1,7 +1,7 @@
 return {
     descriptions = {
-        Back={},
-        Blind={
+        Back = {},
+        Blind = {
             bl_cbean_colon_treasure = {
                 name = "The Treasure",
                 text = {
@@ -27,52 +27,62 @@ return {
                 text = {}
             }
         },
-        Edition={},
-        Enhanced={},
-        Joker={
+        Edition = {},
+        Enhanced = {},
+        Joker = {
             j_cbean_colon_soph = {
                 name = "Sophron",
-                text = {"{C:attention}Retrigger{} each played card", "for each card to the left of it"},
+                text = {
+                    "{C:attention}Retrigger{} each played card",
+                    "for each card to the left of it"
+                },
             },
             j_cbean_colon_plato = {
                 name = "Plato",
-                text = {"All {C:attention}CEO Blinds{} are replaced by ", "a random {V:1}Lowercase Greek Blind"},
+                text = {
+                    "All {C:attention}CEO Blinds{} are replaced by ",
+                    "a random {V:1}Lowercase Greek Blind"
+                },
             },
             j_cbean_colon_aris = {
                 name = "Aristotle",
-                text = {"Open 1 {C:dark_edition}Blessing{} {C:attention}Pack{} when a {C:attention}CEO Blind{} is defeated", "When a {C:dark_edition}Blessing{} is obtained from a {C:attention}Booster Pack{},", "create 1 {C:spectral}Taa Marbuta{}"}, -- Might require to be split somewhere
+                text = {
+                    "Open 1 {C:dark_edition}Blessing{} {C:attention}Pack{} when a {C:attention}CEO Blind{} is defeated",
+                    "When a {C:dark_edition}Blessing{} is obtained from a {C:attention}Booster Pack{},",
+                    "create 1 {C:spectral}Taa Marbuta{}"
+                }, -- Might require to be split somewhere
             },
         },
-        Other={},
-        Planet={},
-        Spectral={},
-        Stake={},
-        Tag={},
-        Tarot={},
-        Voucher={},
+        Other = {},
+        Planet = {},
+        Spectral = {},
+        Stake = {},
+        Tag = {},
+        Tarot = {},
+        Voucher = {},
     },
     misc = {
-        achievement_descriptions={},
-        achievement_names={},
-        blind_states={},
-        challenge_names={},
-        collabs={},
-        dictionary={},
-        high_scores={},
-        labels={},
-        poker_hand_descriptions={},
-        poker_hands={},
-        quips={},
-        ranks={},
-        suits_plural={},
-        suits_singular={},
-        tutorial={},
-        v_dictionary={
+        achievement_descriptions = {},
+        achievement_names = {},
+        blind_states = {},
+        challenge_names = {},
+        collabs = {},
+        dictionary = {},
+        high_scores = {},
+        labels = {},
+        poker_hand_descriptions = {},
+        poker_hands = {},
+        quips = {},
+        ranks = {},
+        suits_plural = {},
+        suits_singular = {},
+        tutorial = {},
+        v_dictionary = {
             coldbean_art = { "Art: #1#" },
             coldbean_code = { "Code: #1#" },
             coldbean_team = { "Team: #1#" },
             coldbean_idea = {"Idea: #1#"}
         },
-        v_text={},
+        v_text = {},
     },
 }
