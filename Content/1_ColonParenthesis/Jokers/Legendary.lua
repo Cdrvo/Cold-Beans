136fc75950d5f3ca76765d8c89a9e9bde412da28
BitterrDoes <bitterdoes420@gmail.com>
2025-11-08 09:15:37 +1100

    Sophron Finished

--- Content/1_ColonParenthesis/Jokers/Legendary.lua
+++ Content/1_ColonParenthesis/Jokers/Legendary.lua
@@ -11,8 +11,18 @@ SMODS.Joker {
     blueprint_compat = true,
 
     calculate = function(self,card,context)
-        if context.individual then
-            
+        if context.repetition and context.cardarea == G.play and not context.end_of_round then
+            local times = 0
+            for i, other_card in pairs(G.play.cards) do
+                if other_card == context.other_card then
+                    times = i-1
+                end
+            end
+            print(times)
+            if times < 1 then return end
+            return {
+                repetitions = times
+            }
         end
     end
 }
@@ -23,6 +33,14 @@ SMODS.Joker {
     rarity = 4,
     price = 13,
     blueprint_compat = true,
+
+    loc_vars = function()
+        return {vars = {
+            colours = {
+                HEX("87a365"), -- colour text formatted with {V:1}
+            },
+        }}
+    end,
 
     calculate = function(self,card,context)
         if context.individual then
--- localization/en-us.lua
+++ localization/en-us.lua
@@ -7,15 +7,15 @@ return {
         Joker={
             j_cbean_colon_soph = {
                 name = "Sophron",
-                text = {"{c:attention}Retrigger{} each played card", "for each card to the left of it"},
+                text = {"{C:attention}Retrigger{} each played card", "for each card to the left of it"},
             },
             j_cbean_colon_plato = {
                 name = "Plato",
-                text = {"All CEO Blinds are replaced by a random Lowercase Greek Blind"},
+                text = {"All {C:attention}CEO Blinds{} are replaced by ", "a random {V:1}Lowercase Greek Blind"},
             },
             j_cbean_colon_aris = {
                 name = "Aristotle",
-                text = {"Open 1 Blessing Pack when a CEO Blind is defeated", "When a Blessing is obtained from a Booster Pack, create 1 Taa Marbuta"}, -- Might require to be split somewhere
+                text = {"Open 1 {C:dark_edition}Blessing{} {C:attention}Pack{} when a {C:attention}CEO Blind{} is defeated", "When a {C:dark_edition}Blessing{} is obtained from a {C:attention}Booster Pack{},", "create 1 {C:spectral}Taa Marbuta{}"}, -- Might require to be split somewhere
             },
         },
         Other={},
