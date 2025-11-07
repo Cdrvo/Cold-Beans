bb42b2c7887683ba150b6e671fab56bef8a8c3aa
kierkat10 <kiernan.simpson@gmail.com>
2025-11-07 16:34:55 -0700

    greek stuff

---
+++ Content/1_ColonParenthesis/2_Blinds/Greek.lua
@@ -0,0 +1,15 @@
+Colonparen.GreekBlind{
+    key = "lowercase_alpha",
+    name = "The Alpha",
+    atlas = "colon_GreekLowercaseBlind",
+    mult = 1,
+    boss_colour = HEX("000000")
+}
+
+Colonparen.GreekBlind{
+    key = "uppercase_alpha",
+    name = "The Alpha",
+    atlas = "colon_GreekUppercaseBlind",
+    mult = 1,
+    boss_colour = HEX("000000")
+}
--- Content/1_ColonParenthesis/Atlases.lua
+++ Content/1_ColonParenthesis/Atlases.lua
@@ -15,3 +15,21 @@ SMODS.Atlas({
 	py = 34,
 	frames = 21,
 })
+
+SMODS.Atlas({
+	key = "colon_LowercaseGreekBlind",
+	atlas_table = "ANIMATION_ATLAS",
+	path = "1_ColonParenthesis/LowercaseGreekBlind.png",
+	px = 34,
+	py = 34,
+	frames = 21,
+})
+
+SMODS.Atlas({
+	key = "colon_UppercaseGreekBlind",
+	atlas_table = "ANIMATION_ATLAS",
+	path = "1_ColonParenthesis/UppercaseGreekBlind.png",
+	px = 34,
+	py = 34,
+	frames = 21,
+})
