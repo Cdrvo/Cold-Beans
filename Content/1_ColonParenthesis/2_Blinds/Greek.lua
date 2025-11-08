dd9af4c899c4290d99a8b9d4597440b193ccb76e
kierkat10 <kiernan.simpson@gmail.com>
2025-11-07 17:00:39 -0700

    placeholders for greek blinds

--- Content/1_ColonParenthesis/2_Blinds/Greek.lua
+++ Content/1_ColonParenthesis/2_Blinds/Greek.lua
@@ -2,8 +2,92 @@
 Colonparen.GreekBlind{
     key = "alpha",
     name = "The Alpha",
-    atlas = "colon_GreekLowercaseBlind",
     mult = 1,
-    boss_colour = HEX("000000")
+    -- boss_colour = HEX("000000"),
+    lower = {
+        atlas = "colon_GreekLowercaseBlind",
+        pos = { x = 0, y = 0 },
+        set_blind = function(self, card, from_blind)
+        end,
+        calculate = function(self, blind, context)
+        end
+    },
+    upper = {
+        atlas = "colon_GreekUppercaseBlind",
+        pos = { x = 0, y = 0 },
+        set_blind = function(self, card, from_blind)
+        end,
+        calculate = function(self, blind, context)
+        end
+    }
+}
+
+Colonparen.GreekBlind{
+    key = "beta",
+    name = "The Beta",
+    mult = 1,
+    -- boss_colour = HEX("000000"),
+    lower = {
+        atlas = "colon_GreekLowercaseBlind",
+        pos = { x = 0, y = 0 },
+        set_blind = function(self, card, from_blind)
+        end,
+        calculate = function(self, blind, context)
+        end
+    },
+    upper = {
+        atlas = "colon_GreekUppercaseBlind",
+        pos = { x = 0, y = 0 },
+        set_blind = function(self, card, from_blind)
+        end,
+        calculate = function(self, blind, context)
+        end
+    }
+}
+
+Colonparen.GreekBlind{
+    key = "gamma",
+    name = "The Gamma",
+    mult = 1,
+    -- boss_colour = HEX("000000"),
+    lower = {
+        atlas = "colon_GreekLowercaseBlind",
+        pos = { x = 0, y = 0 },
+        set_blind = function(self, card, from_blind)
+        end,
+        calculate = function(self, blind, context)
+        end
+    },
+    upper = {
+        atlas = "colon_GreekUppercaseBlind",
+        pos = { x = 0, y = 0 },
+        set_blind = function(self, card, from_blind)
+        end,
+        calculate = function(self, blind, context)
+        end
+    }
+}
+
+Colonparen.GreekBlind{
+    key = "delta",
+    name = "The Delta",
+    mult = 1,
+    -- boss_colour = HEX("000000"),
+    lower = {
+        atlas = "colon_GreekLowercaseBlind",
+        pos = { x = 0, y = 0 },
+        set_blind = function(self, card, from_blind)
+        end,
+        calculate = function(self, blind, context)
+        end
+    },
+    upper = {
+        atlas = "colon_GreekUppercaseBlind",
+        pos = { x = 0, y = 0 },
+        set_blind = function(self, card, from_blind)
+        end,
+        calculate = function(self, blind, context)
+        end
+    }
 }
 ]]
