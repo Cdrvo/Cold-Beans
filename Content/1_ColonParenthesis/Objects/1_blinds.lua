c4742725d39690210e75ea6b7850b1c0564c3b88
justamirror <66569648+justamirror@users.noreply.github.com>
2025-11-08 11:40:11 -0500

    reorganize this bullshit i can't work like this lmao

--- Content/1_ColonParenthesis/Objects/1_blinds.lua
+++ Content/1_ColonParenthesis/Objects/1_blinds.lua
@@ -387,3 +387,53 @@ function SMODS.collection_pool(item, ...)
 	return old_collection_pool(item, ...)
 end
 
+
+                                                                        -- Blind shit
+local G_UIDEF_current_blinds = G.UIDEF.current_blinds;
+function G.UIDEF.current_blinds()
+	local value = G_UIDEF_current_blinds();
+	table.insert(value.nodes, 1, G.GAME.round_resets.blind_states['Teeny'] ~= 'Hide' and {n=G.UIT.C, config={align = "tm", padding = 0.1, outline = 2, r = 0.1, line_emboss = 0.2, outline_colour = G.C.BLACK}, nodes={
+      create_UIBox_blind_choice('Teeny', true)
+    }} or nil)
+	value.nodes[#value.nodes+1] = G.GAME.round_resets.blind_states['CEO'] ~= 'Hide' and {n=G.UIT.C, config={align = "tm", padding = 0.1, outline = 2, r = 0.1, line_emboss = 0.2, outline_colour = G.C.BLACK}, nodes={
+      create_UIBox_blind_choice('CEO', true)
+    }} or nil;
+	return value;
+end
+
+local set_blind = Blind.set_blind
+function Blind:set_blind(blind, reset, silent)
+	if not reset then
+		self.colonparen_blindtype = (blind or {}).colonparen_blindtype;
+		if self.colonparen_blindtype == nil then
+			if self.name == "Small Blind" then
+				self.colonparen_blindtype = 'Small'
+			elseif self.name == "Big Blind" then 
+				self.colonparen_blindtype = 'Big'
+			elseif self.name and self.name ~= '' then
+				self.colonparen_blindtype = 'Boss'
+			end
+		end
+	end
+	return set_blind(self, blind, reset, silent)
+end
+for i, name in ipairs({'Teeny', 'Small', 'Big', 'CEO'}) do
+	local P_STRING = 'P_' ..  (name:upper()) .. '_BLINDS'
+	G[P_STRING] = {}
+	Colonparen[name .. 'Blind'] = SMODS.Blind:extend {
+        get_obj = function(self, key) return G[P_STRING][key] end,
+        register = function(self)
+            self.name = self.name or self.key
+			self.colonparen_blindtype = name;
+            SMODS.Blind.super.register(self)
+			Colonparen.SpecialBlinds[self.key .. "_" .. name] = self;
+        end,
+        inject = function(self, i)
+            -- no pools to query length of, so we assign order manually
+            if not self.taken_ownership then
+                self.order = 30 + i
+            end
+            G[P_STRING][self.key] = self
+        end
+    }
+end
