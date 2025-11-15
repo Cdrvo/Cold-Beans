8a92c60e9b9b974ca6952f0b0d858e284f7e98ab
f-raZ0R <MonkeyCityOwner@gmail.com>
2025-11-15 10:01:11 -0500

    ok so the hand doesn't crash anymore at least? none of the cards inside of it are scoring though....

--- Content/2_SeriousDownloading/items/handtypes/bobtail.lua
+++ Content/2_SeriousDownloading/items/handtypes/bobtail.lua
@@ -1,3 +1,30 @@
+SMODS.PokerHandPart {
+    key = 'sdown_bobtail',
+    func = function(hand)
+        --eject early if hand is too small
+        if #hand < (SMODS.four_fingers('straight') -1) then return {} end
+        straightsTable = get_straight(hand, SMODS.four_fingers('straight') -1, SMODS.shortcut(), SMODS.wrap_around_straight())
+        if next(straightsTable) == nil then return {} end
+        bobtails = {}
+        for _,straight in pairs(straightsTable) do
+            --have to reimplement the flush calc because unlike get_straight, smods doesn't modify it
+            for suit,_ in pairs(SMODS.Suits) do
+                local t = {}
+                local flush_count = 0
+                for i,_ in pairs(straight) do
+                    if straight[i]:is_suit(suit, nil, true) then flush_count = flush_count + 1;  t[#t+1] = straight[i] end
+                end
+                if flush_count >= (SMODS.four_fingers('flush') - 1) then
+                    table.insert(bobtails, t)
+                end
+            end
+        end
+        if next(bobtails) == nil then return {} end
+        return { bobtails }
+    end
+}
+
+
 SMODS.PokerHand{
     key = "sdown_bobtail",
     l_chips = 30,
@@ -13,27 +40,6 @@ SMODS.PokerHand{
         { "S_5", false },
     },
     evaluate = function(parts, hand)
-        --eject early if hand is too small
-        if #hand < (SMODS.four_fingers('straight') -1) then return {} end
-        straightsTable = get_straight(hand, SMODS.four_fingers('straight') -1, SMODS.shortcut(), SMODS.wrap_around_straight())
-        if next(straightsTable) == nil then return {} end
-        bobtails = {}
-        for _,straight in pairs(straightsTable) do
-            --have to reimplement the flush calc because unlike get_straight, smods doesn't modify it
-            for suit,_ in pairs(SMODS.Suits) do
-                local t = {}
-                local flush_count = 0
-                --i do not understand. the print shows that
-                --print(straight)
-                for i,_ in pairs(straight) do
-                    if straight[i]:is_suit(suit, nil, true) then flush_count = flush_count + 1;  t[#t+1] = straight[i] end
-                end
-                if flush_count >= (SMODS.four_fingers('flush') - 1) then
-                    table.insert(bobtails, t)
-                end
-            end
-        end
-        if next(bobtails) == nil then return {} end
-        return { bobtails }
+        return next(parts.cbean_sdown_bobtail) and parts.cbean_sdown_bobtail or {}
     end
 }
