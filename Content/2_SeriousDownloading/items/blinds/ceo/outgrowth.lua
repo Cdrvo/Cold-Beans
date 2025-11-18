3f717e80c6c3ab6fa4ab27307e1a039f31bfc78f
f-raZ0R <MonkeyCityOwner@gmail.com>
2025-11-18 11:37:56 -0500

    The Outgrowth

--- Content/2_SeriousDownloading/items/blinds/ceo/outgrowth.lua
+++ Content/2_SeriousDownloading/items/blinds/ceo/outgrowth.lua
@@ -6,6 +6,15 @@ Colonparen.CEOBlind {
     atlas = "colon_CEOBlind",
     mult = 3,
     boss_colour = HEX("5933BB"),
+    debuff_hand = function(self, cards, hand, handname, check)
+        local _,_,_,scoring_hand,_ = G.FUNCS.get_poker_hand_info(cards)
+        local always_scores_count = 0
+        for _, card in pairs(cards) do
+            if SMODS.always_scores(card) then always_scores_count = always_scores_count + 1 end
+        end
+        if #scoring_hand + always_scores_count ~= (#cards - 1) then return true end
+    end,
+
     beans_credits = {
         team = "SeriousDownloading",
         idea = "Athebyne",
