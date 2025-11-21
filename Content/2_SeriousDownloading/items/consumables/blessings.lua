3d18104f312865050da4be93654612167d56fc63
wingedcatgirl <69098783+wingedcatgirl@users.noreply.github.com>
2025-11-20 21:56:41 -0500

    oh yeah, credit

--- Content/2_SeriousDownloading/items/consumables/blessings.lua
+++ Content/2_SeriousDownloading/items/consumables/blessings.lua
@@ -417,7 +417,7 @@ SMODS.Consumable {
         team = "SeriousDownloading",
         idea = "kars",
         art = "",
-        code = "",
+        code = "mys. minty",
     },
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.times_left, card.ability.extra.dollars } }
