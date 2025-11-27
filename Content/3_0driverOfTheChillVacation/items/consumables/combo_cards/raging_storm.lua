b1fe0bfae5b3c2407424886abfd90b4d6ffaae6b
MarioFan597 <81440403+MarioFan597@users.noreply.github.com>
2025-11-27 01:30:18 -0500

    Combo cards are now seperated for clarity's sake

---
+++ Content/3_0driverOfTheChillVacation/items/consumables/combo_cards/raging_storm.lua
@@ -0,0 +1,62 @@
+SMODS.Consumable {
+    key = '0chill_punch',
+    set = 'Combo', --Had to leave out team name since the 0 caused issues
+    atlas = '0chill_combo_atlas',
+    config = { 
+         immutable = {
+            ---------------------- What every combo card needs
+            combo_type = "starter",
+            sequence = 0
+            ----------------------
+        },
+        extra = {
+            mult = 10
+        },
+        extra_slots_used = -0.75
+    },
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.mult} }
+    end,
+    pos = { x = 2, y = 0 },
+    can_use = function(self, card)
+        return true
+    end,
+    use = function(self, card, area, copier) --Each card will have two uses. The first is when it is selected  and the second is when it is de-selected
+
+        if G.GAME.blind.in_blind and CanCombo(card) --Selecting Card
+        then
+            SelectCombo(card)
+        elseif G.GAME.blind.in_blind and CanUncombo(card) then --Deselecting Card
+            UnselectCombo(card)
+        else
+            return nil
+        end              
+    end,                                 
+    keep_on_use = function(self, card) --Needed for every combo card
+        return true
+    end,
+    calculate = function(self, card, context)
+        if context.joker_main and card.ability.immutable.sequence > 0 then
+            return {
+                mult = (card.ability.extra.mult * (1 + (card.ability.immutable.sequence - 1)/10 )) --Combo effects power
+            }
+        end
+        if context.after and card.ability.immutable.sequence > 0 then
+            SMODS.destroy_cards(card, nil, nil, true)
+            G.GAME.cbean_combos_used = G.GAME.cbean_combos_used + 1
+        end
+    end,
+    remove_from_deck = function(self, card, from_debuff) 
+        UnselectCombo(card)
+    end,
+    beans_credits = {
+        team = {"0 Driver Of",
+                "The Chill Vacation"
+                },
+        idea = "MarioFan597", --TODO
+        art = "cmykl",  --TODO
+        code = {"MarioFan597",
+                "Inspector_B"
+                },
+    },
+}
---
+++ Content/3_0driverOfTheChillVacation/items/consumables/combo_cards/standing_knee.lua
@@ -0,0 +1,65 @@
+SMODS.Consumable {
+    key = '0chill_standing_knee',
+    set = 'Combo', --Had to leave out team name since the 0 caused issues
+    atlas = '0chill_combo_atlas',
+    config = { 
+         immutable = {
+            ---------------------- What every combo card needs
+            combo_type = "starter",
+            sequence = 0
+            ----------------------
+        },
+        extra = {
+            mult = 20
+        },
+        extra_slots_used = -0.75
+    },
+    loc_vars = function(self, info_queue, card)
+        info_queue[#info_queue + 1] = { key = "cbean_combo_starter", set = "Other" }
+        return { vars = { card.ability.extra.mult} }
+    end,
+    pos = { x = 6, y = 0 },
+    can_use = function(self, card)
+        return true
+    end,
+    use = function(self, card, area, copier) --Each card will have two uses. The first is when it is selected  and the second is when it is de-selected
+
+        if G.GAME.blind.in_blind and CanCombo(card) --Selecting Card
+        then
+            SelectCombo(card)
+        elseif G.GAME.blind.in_blind and CanUncombo(card) then --Deselecting Card
+            UnselectCombo(card)
+        else
+            return nil
+        end              
+    end,                                 
+    keep_on_use = function(self, card) --Needed for every combo card
+        return true
+    end,
+    calculate = function(self, card, context)
+        if context.individual and context.cardarea == G.play and card.ability.immutable.sequence > 0 then
+            if(context.other_card == context.scoring_hand[1]) then
+                return {
+                    mult = (card.ability.extra.mult * (1 + (card.ability.immutable.sequence - 1)/10 ))
+                }
+            end
+        end
+        if context.after and card.ability.immutable.sequence > 0 then
+            SMODS.destroy_cards(card, nil, nil, true)
+            G.GAME.cbean_combos_used = G.GAME.cbean_combos_used + 1
+        end
+    end,
+    remove_from_deck = function(self, card, from_debuff) 
+        UnselectCombo(card)
+    end,
+    beans_credits = {
+        team = {"0 Driver Of",
+                "The Chill Vacation"
+                },
+        idea = "CapitalChirp", 
+        art = "cmykl", 
+        code = {"MarioFan597",
+                "Inspector_B"
+                },
+    },
+}
