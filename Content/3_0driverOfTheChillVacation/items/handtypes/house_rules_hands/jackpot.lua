680be3c376aa4ac9aeb4b025bab1429155fe5834
Aaron Bowden <email@example.com>
2025-11-28 14:22:48 -0600

    Add Jackpot

---
+++ Content/3_0driverOfTheChillVacation/items/handtypes/house_rules_hands/jackpot.lua
@@ -0,0 +1,50 @@
+
+-- Jackpot
+SMODS.PokerHandPart { --Modifed from Potassium Remake
+    key = "0chill_jackpot",
+    func = function (hand)
+        if #hand < 3 then return {} end
+
+        local track_ranks = {}
+
+        for _,card in ipairs(hand) do
+            local rank = card:get_id()
+            track_ranks[rank] = track_ranks[rank] or {}
+            table.insert(track_ranks[rank], card)
+        end
+
+        if not (
+            -- A rank is not tracked if the rank is not in the hand
+            track_ranks[7]     
+            and track_ranks[7] 
+            and track_ranks[7] 
+        ) then return {} end
+
+        local scoring_cards = SMODS.merge_lists{
+            track_ranks[7],
+            track_ranks[7],
+            track_ranks[7], 
+        }
+
+        return {scoring_cards}
+    end
+}
+
+SMODS.PokerHand{
+    key = "0chill_jackpot",
+    l_chips = 37,
+    l_mult = 4,
+    chips = 77,
+    mult = 7,
+    visible = false,
+    example = {
+        { "S_7", true },
+        { "D_7", true },
+        { "H_7", true },
+    },
+    evaluate = function(parts, hand)
+        if (#SMODS.find_card('j_cbean_0chill_house_rules') > 0) and (#SMODS.find_card('j_oops') > 0) then --Checks if house rules and required joker are owned
+            return parts.cbean_0chill_jackpot
+        end
+    end
+}
