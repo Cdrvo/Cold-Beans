dbbd1ff7ea4712dd55b53c71c7f594665a9991ca
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-12-10 06:37:41 -0500

    UI Changes

--- Content/5_Yeah!MostlyArtists/consumables/Keys/ui.lua
+++ Content/5_Yeah!MostlyArtists/consumables/Keys/ui.lua
@@ -1,53 +1,108 @@
+G.STATES.MAIN_STREET = 24783948327
 G.STATES.GRAVEYARD = 1366468461
+
+G.FUNCS.show_yma_main_street = function(e)
+  stop_use()
+  hide_location(G.shop)
+  
+  G.STATE = G.STATES.MAIN_STREET
+  G.STATE_COMPLETE = false
+  show_location(G.main_street)
+
+  local sign_sprite = G.SHOP_SIGN.UIRoot.children[1].children[1].children[1].config.object
+  ease_background_colour_blind(G.STATE)
+  sign_sprite.states.visible = false
+  cached_hand_state = G.hand.states.visible
+  G.hand.states.visible = false
+end
+
+G.FUNCS.hide_yma_main_street = function(e)
+    stop_use()
+	hide_location(G.main_street)
+	G.hand.states.visible = cached_hand_state
+	G.STATE = G.STATES.SHOP
+	G.STATE_COMPLETE = false
+    show_location(G.shop)
+
+	local sign_sprite = G.SHOP_SIGN.UIRoot.children[1].children[1].children[1].config.object
+	ease_background_colour_blind(G.STATES.SHOP)
+	sign_sprite.atlas = G.ANIMATION_ATLAS["shop_sign"]
+    sign_sprite.states.visible = true
+end
+
 
 G.FUNCS.show_yma_graveyard = function(e)
   stop_use()
-  hide_shop()
+  hide_location(G.main_street)
   
   G.STATE = G.STATES.GRAVEYARD
   G.STATE_COMPLETE = false
-  if G.graveyard and G.graveyard.alignment.offset.py then 
-        G.graveyard.alignment.offset.y = G.graveyard.alignment.offset.py
-        G.graveyard.alignment.offset.py = nil
-  end
+  
 
   local sign_sprite = G.SHOP_SIGN.UIRoot.children[1].children[1].children[1].config.object
   ease_background_colour_blind(G.STATE)
   sign_sprite.atlas = G.ANIMATION_ATLAS["cbean_pboys_backalley_shop"]
-  cached_hand_state = G.hand.states.visible
   G.hand.states.visible = false
-
+  sign_sprite.states.visible = true
+  show_location(G.graveyard)
 end
 
 G.FUNCS.hide_yma_graveyard = function(e)
-	stop_use()
-    if G.graveyard then 
-      G.E_MANAGER:add_event(Event({
-        trigger = 'immediate',
-        func = function()
-          G.graveyard.alignment.offset.y = G.ROOM.T.y + 29
-          return true
-        end
-      })) 
-      --G.E_MANAGER:add_event(Event({
-      --  trigger = 'after',
-      --  delay = 0.5,
-      --  func = function()
-      --    if G.graveyard then
-      --      G.graveyard:remove()
-      --      G.graveyard = nil
-      --    end
-      --    return true
-      --  end
-      --}))
-    end
-	G.hand.states.visible = cached_hand_state
-	G.STATE = G.STATES.SHOP
+    stop_use()
+	hide_location(G.graveyard)
+	G.STATE = G.STATES.MAIN_STREET
 	G.STATE_COMPLETE = false
+    
 	local sign_sprite = G.SHOP_SIGN.UIRoot.children[1].children[1].children[1].config.object
-	ease_background_colour_blind(G.STATES.SHOP)
+	ease_background_colour_blind(G.STATES.MAIN_STREET)
 	sign_sprite.atlas = G.ANIMATION_ATLAS["shop_sign"]
-	show_shop()
+    show_location(G.main_street)
+end
+
+function update_main_street()
+    if not G.STATE_COMPLETE then
+        stop_use()
+        ease_background_colour_blind(G.STATES.MAIN_STREET)
+        local main_street_exists = not not G.main_street
+        G.main_street = G.main_street or UIBox{
+            definition = G.UIDEF.yma_main_street(),
+            config = {align='tmi', offset = {x=0,y=G.ROOM.T.y+20},major = G.hand, bond = 'Weak'}
+        }
+        G.main_street:recalculate()
+        if G.yma_mainstreet_graveyard then
+            if #SMODS.find_card("c_cbean_yma_moon") >= 1 and G.GAME and G.GAME.cbean and #G.GAME.cbean.destroyed_jokers > 0 then
+                G.yma_mainstreet_graveyard.states.visible = true
+            else
+                G.yma_mainstreet_graveyard.states.visible = false
+            end
+            G.yma_mainstreet_graveyard:recalculate()
+        end
+
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                G.main_street.alignment.offset.y = -5.3
+                G.E_MANAGER:add_event(Event({
+                    trigger = 'after',
+                    delay = 0.2,
+                    blockable = false,
+                    func = function()
+                        if math.abs(G.main_street.T.y - G.main_street.VT.y) < 3 then
+                            local nosave_main_street = nil 
+                            -- Back to shop button
+							play_sound('cbean_pboy_bavoice', G.SPEEDFACTOR*(math.random()*0.4+2), 2.5)
+                            G.CONTROLLER:snap_to({node = G.main_street:get_UIE_by_ID('shop_button')})
+                            -- not loaded from save?
+                            if not nosave_main_street then G.E_MANAGER:add_event(Event({ func = function() save_run(); return true end})) end
+                            return true
+                        end
+                    end}))
+                return true
+            end
+        }))
+
+
+        G.STATE_COMPLETE = true
+    end
 end
 
 function update_graveyard()
@@ -85,7 +140,73 @@ function update_graveyard()
 
         G.STATE_COMPLETE = true
     end
+end
 
+local lcpref2 = Controller.L_cursor_press
+function Controller:L_cursor_press(x, y)
+    lcpref2(self, x, y)
+	if G.STATE == G.STATES.MAIN_STREET and G.main_street and G.GAME then
+        if G.yma_mainstreet_alleyway and G.yma_mainstreet_alleyway.states.collide.is then
+            G.FUNCS.show_balley()
+        elseif G.yma_mainstreet_graveyard and G.yma_mainstreet_graveyard.states.collide.is and #SMODS.find_card("c_cbean_yma_moon") >= 1 and G.GAME.cbean and #G.GAME.cbean.destroyed_jokers > 0 then
+            G.FUNCS.show_yma_graveyard()
+		end
+	end
+end
+
+function G.UIDEF.yma_main_street()
+    
+    G.yma_mainstreet_alleyway = UIBox{
+        definition = G.UIDEF.alleysprite(),
+        config = {align='cm', offset = {x=0.1,y=-0.4}, major = G.hand, bond = 'Weak', draggable = false, collideable = true, can_collide = true}
+    }
+    G.yma_mainstreet_graveyard = UIBox{
+        definition = G.UIDEF.gravesprite(),
+        config = {align='cm', offset = {x=0,y=-0.4}, major = G.hand, bond = 'Weak', draggable = false, collideable = true, can_collide = true}
+    }
+
+    local table = {}
+    table[#table+1] = {
+        n=G.UIT.C, config={align = "cm", padding = 0.15, r=0.2, colour = G.C.DYN_UI.BOSS_MAIN, minh = 3.0, emboss = 0.05}, nodes={
+            {n=G.UIT.O, config={object = G.yma_mainstreet_alleyway}},
+        }}
+    if G.yma_mainstreet_graveyard.states.visible then
+        table[#table+1] = {n=G.UIT.C, config={align = "cm", padding = 0.15, r=0.2, colour = G.C.DYN_UI.BOSS_MAIN, emboss = 0.05}, nodes={
+            {n=G.UIT.O, config={object = G.yma_mainstreet_graveyard}},
+        }}
+    end
+    
+    local t = {n=G.UIT.ROOT, config = {align = 'cl', colour = G.C.CLEAR}, nodes={
+            UIBox_dyn_container({
+                {n=G.UIT.C, config={align = "cm", padding = 0.1, emboss = 0.05, r = 0.1, colour = G.C.L_BLACK}, nodes={
+                    {n=G.UIT.R, config={align = "cm", padding = 0.05}, nodes=table},
+                    {n=G.UIT.R, config={align = "cm", minh = 3.5}, nodes={}},
+                }
+              },
+              
+              }, false)
+        }}
+    return t
+end
+
+function G.UIDEF.alleysprite()
+  
+	local sprite_alley = G.ASSET_ATLAS and AnimatedSprite(0, 0, (113*0.113)*0.35, (71*0.057)*0.35, G.ANIMATION_ATLAS["cbean_pboys_backalley_shop"], { x = 0, y = 0 }) or nil
+
+    local t = {n=G.UIT.ROOT, config = {align = 'cm', colour = G.C.CLEAR}, nodes={
+            {n=G.UIT.O, config={object = sprite_alley}},
+    }}
+    return t
+end
+
+function G.UIDEF.gravesprite()
+  
+	local sprite_grave = G.ASSET_ATLAS and AnimatedSprite(0, 0, (113*0.113)*0.35, (71*0.057)*0.35, G.ANIMATION_ATLAS["shop_sign"], { x = 0, y = 0 }) or nil
+
+    local t = {n=G.UIT.ROOT, config = {align = 'cm', colour = G.C.CLEAR}, nodes={
+            {n=G.UIT.O, config={object = sprite_grave}},
+    }}
+    return t
 end
 
 function G.UIDEF.yma_graveyard()
