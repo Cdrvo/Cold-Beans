838a0e575d25000ab0c8a0cb717094b8660ca02d
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-12-11 10:32:15 -0500

    Lambda Greek blind

--- Content/5_Yeah!MostlyArtists/blinds/Greek.lua
+++ Content/5_Yeah!MostlyArtists/blinds/Greek.lua
@@ -1,4 +1,63 @@
--- OI! PRETTY MUCH ALL OF THESE ARE UNFINISHED. DW ABOUT IT BECAUSE THE SECOND RUNAROUND EXISTS FOR A REASON - jamirror ( and the rest of Team :( )
+--Greek blinds that weren't coded cause i still had more time
+function yma_redeem_voucher(tier, upgrade)
+    local _pool, _ = get_current_pool('Voucher', nil, nil, nil)
+
+    -- Check if there are any base vouchers that haven't already been redeemed
+    local _bases = 0
+    for i = 1, #_pool do
+    if _pool[i] ~= 'UNAVAILABLE' and (not G.P_CENTERS[_pool[i]].requires or tier ~= 1) then
+        _bases = _bases + 1
+    end
+    end
+    if _bases > 0 then
+    local _pool_key = 'Voucher_fromtag'
+    local _voucher_key = pseudorandom_element(_pool, pseudoseed(_pool_key))
+    local iv = 1
+    while _voucher_key == 'UNAVAILABLE' or (G.P_CENTERS[_voucher_key].requires and tier == 1) do
+        iv = iv + 1
+        _voucher_key = pseudorandom_element(_pool, pseudoseed(_pool_key .. '_resample' .. iv))
+    end
+
+    local vouchers_to_create = { _voucher_key }
+    if upgrade and tier == 1 then
+        for _, v in ipairs(G.P_CENTER_POOLS["Voucher"]) do
+            if v.requires and type(v.requires) == "table" then
+            for _, requirement in ipairs(v.requires) do
+                if requirement == _voucher_key then
+                table.insert(vouchers_to_create, v.key)
+                break
+                end
+            end
+            end
+        end
+    end
+
+    local cards_to_redeem = {}
+    for _, key in ipairs(vouchers_to_create) do
+        local voucher_card = create_card('Voucher', G.hand, nil, nil, nil, nil, key, 'yma_voucher')
+        G.hand:emplace(voucher_card)
+        voucher_card.cost = 0
+        table.insert(cards_to_redeem, voucher_card)
+    end
+    local prev_state = G.STATE
+    delay(0.2)
+    for _, voucher_card in ipairs(cards_to_redeem) do
+        G.STATE = prev_state
+        G.FUNCS.use_card({ config = { ref_table = voucher_card } })
+        delay(0.6)
+    end
+    end
+    G.E_MANAGER:add_event(Event({
+    func = function()
+        for i = 1, #G.GAME.tags do
+        if G.GAME.tags[i]:apply_to_run({ type = 'new_blind_choice' }) then
+            break
+        end
+        end
+        return true
+    end
+    }))
+end
 
 Colonparen.GreekBlind{
     key = "colon_alpha",
@@ -96,6 +155,134 @@ Colonparen.GreekBlind{
                 end
             end
         end
+    },
+    beans_credits = {
+        team = ":( / Yeah! Mostly Artists",
+        idea = "Glitchkat10",
+        art = "George The Rat",
+        code = "RattlingSnow353",
+    }
+}
+
+Colonparen.GreekBlind{
+    key = "colon_iota",
+    name = "Iota",
+    mult = 1,
+    boss_colour = HEX("6cde80"),
+    pos = { x = 0, y = 8 },
+    lower = {
+        set_blind = function(self, card, from_blind)
+        end,
+        calculate = function(self, blind, context)
+        end
+    },
+    upper = {
+        set_blind = function(self, card, from_blind)
+        end,
+        calculate = function(self, blind, context)
+        end
+    },
+    beans_credits = {
+        team = ":( / Yeah! Mostly Artists",
+        idea = "Glitchkat10",
+        art = "George The Rat",
+        code = "RattlingSnow353",
+    }
+}
+
+Colonparen.GreekBlind{
+    key = "colon_lambda",
+    name = "Lambda",
+    mult = 1,
+    boss_colour = HEX("6cdea3"),
+    pos = { x = 0, y = 10 },
+    lower = {
+        set_blind = function(self, card, from_blind)
+            yma_redeem_voucher(1)
+            yma_redeem_voucher(1)
+        end,
+    },
+    upper = {
+        set_blind = function(self, card, from_blind)
+            yma_redeem_voucher(1, true)
+            yma_redeem_voucher(1, true)
+        end,
+    },
+    beans_credits = {
+        team = ":( / Yeah! Mostly Artists",
+        idea = "Glitchkat10",
+        art = "George The Rat",
+        code = "RattlingSnow353",
+    }
+}
+
+Colonparen.GreekBlind{
+    key = "colon_mu",
+    name = "Mu",
+    mult = 1,
+    boss_colour = HEX("6cdec6"),
+    pos = { x = 0, y = 11 },
+    lower = {
+        set_blind = function(self, card, from_blind)
+        end,
+    },
+    upper = {
+        set_blind = function(self, card, from_blind)
+        end,
+    },
+    beans_credits = {
+        team = ":( / Yeah! Mostly Artists",
+        idea = "Glitchkat10",
+        art = "George The Rat",
+        code = "RattlingSnow353",
+    }
+}
+
+Colonparen.GreekBlind{
+    key = "colon_omicron",
+    name = "Omicron",
+    mult = 1,
+    boss_colour = HEX("6cbfde"),
+    pos = { x = 0, y = 14 },
+    lower = {
+    },
+    upper = {
+    },
+    beans_credits = {
+        team = ":( / Yeah! Mostly Artists",
+        idea = "Glitchkat10",
+        art = "George The Rat",
+        code = "RattlingSnow353",
+    }
+}
+
+Colonparen.GreekBlind{
+    key = "colon_pi",
+    name = "Pi",
+    mult = 1,
+    boss_colour = HEX("6cabde"),
+    pos = { x = 0, y = 15 },
+    lower = {
+    },
+    upper = {
+    },
+    beans_credits = {
+        team = ":( / Yeah! Mostly Artists",
+        idea = "Glitchkat10",
+        art = "George The Rat",
+        code = "RattlingSnow353",
+    }
+}
+
+Colonparen.GreekBlind{
+    key = "colon_rho",
+    name = "Rho",
+    mult = 1,
+    boss_colour = HEX("6c97de"),
+    pos = { x = 0, y = 16 },
+    lower = {
+    },
+    upper = {
     },
     beans_credits = {
         team = ":( / Yeah! Mostly Artists",
