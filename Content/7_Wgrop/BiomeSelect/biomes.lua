43e31a60bc864b6fc80909fb862fe573a6b1d16b
Luna Astra Cassiopeia <kadabe380@gmail.com>
2025-12-25 05:59:41 +0800

    biome calcs

--- Content/7_Wgrop/BiomeSelect/biomes.lua
+++ Content/7_Wgrop/BiomeSelect/biomes.lua
@@ -56,7 +56,10 @@ CBWG.ColdBeans_Biome {
     atlas = 'wgrop_biomes',
     key = "wgrop_desert",
     blinds = {["bl_wheel"] = true, ["bl_mark"] = true, ["bl_hook"] = true, ["bl_manacle"] = true, ["bl_window"] = true, ["bl_final_acorn"] = true, ["bl_cbean_colon_pot"] = true},
-    cards = {["j_greedy_joker"] = true, ["j_dusk"] = true, ["j_delayed_grat"] = true, ["j_obelisk"] = true, ["j_hallucination"] = true, ["j_fortune_teller"] = true, ["j_bull"] = true, ["j_cartomancer"] = true, ["j_bootstraps"] = true, ["j_cbean_wgrop_gunslinger"] = true, ["j_cbean_wgrop_oasis"] = true, ["j_cbean_wgrop_glass_spire"] = true, ["j_mystic_summit"] = true, ["j_rough_gem"] = true}
+    cards = {["j_greedy_joker"] = true, ["j_dusk"] = true, ["j_delayed_grat"] = true, ["j_obelisk"] = true, ["j_hallucination"] = true, ["j_fortune_teller"] = true, ["j_bull"] = true, ["j_cartomancer"] = true, ["j_bootstraps"] = true, ["j_cbean_wgrop_gunslinger"] = true, ["j_cbean_wgrop_oasis"] = true, ["j_cbean_wgrop_glass_spire"] = true, ["j_mystic_summit"] = true, ["j_rough_gem"] = true},
+    calculate = function(self, context)
+
+    end
 }
 
 CBWG.ColdBeans_Biome {
@@ -65,7 +68,10 @@ CBWG.ColdBeans_Biome {
     atlas = 'wgrop_biomes',
     key = "wgrop_forest",
     blinds = {["bl_ox"] = true, ["bl_plant"] = true, ["bl_club"] = true,  ["bl_serpent"] = true, ["bl_final_leaf"] = true, ["bl_cbean_colon_treasure"] = true, ["bl_cbean_colon_seed"] = true, ["bl_cbean_colon_compass"] = true, ["bl_cbean_colon_outcrop"] = true},
-    cards = {["j_gluttenous_joker"] = true, ["j_stencil"] = true, ["j_ceremonial"] = true, ["j_marble"] = true, ["j_scary_face"] = true, ["j_gros_michel"] = true, ["j_egg"] = true, ["j_sixth_sense"] = true, ["j_hiker"] = true, ["j_faceless"] = true, ["j_green_joker"] = true, ["j_cavendish"] = true, ["j_midas_mask"] = true, ["j_erosion"] = true, ["j_ancient"] = true, ["j_campfire"] = true, ["j_flower_pot"] = true, ["j_idol"] = true, ["j_hit_the_road"] = true, ["j_cbean_colon_square_packing"] = true, ["j_cbean_pboys_watermelon"] = true, ["j_cbean_nameteam_bottomofthebarrel"] = true, ["j_golden"] = true, ["j_arrowhead"] = true}
+    cards = {["j_gluttenous_joker"] = true, ["j_stencil"] = true, ["j_ceremonial"] = true, ["j_marble"] = true, ["j_scary_face"] = true, ["j_gros_michel"] = true, ["j_egg"] = true, ["j_sixth_sense"] = true, ["j_hiker"] = true, ["j_faceless"] = true, ["j_green_joker"] = true, ["j_cavendish"] = true, ["j_midas_mask"] = true, ["j_erosion"] = true, ["j_ancient"] = true, ["j_campfire"] = true, ["j_flower_pot"] = true, ["j_idol"] = true, ["j_hit_the_road"] = true, ["j_cbean_colon_square_packing"] = true, ["j_cbean_pboys_watermelon"] = true, ["j_cbean_nameteam_bottomofthebarrel"] = true, ["j_golden"] = true, ["j_arrowhead"] = true},
+    calculate = function(self, context)
+        
+    end
 }
 
 CBWG.ColdBeans_Biome {
@@ -74,7 +80,28 @@ CBWG.ColdBeans_Biome {
     atlas = 'wgrop_biomes',
     key = "wgrop_graveyard",
     blinds = {["bl_eye"] = true, ["bl_final_vessel"] = true, ["bl_cbean_colon_assassination"] = true, ["bl_cbean_colon_hole"] = true, ["bl_cbean_colon_curse"] = true, ["bl_cbean_colon_regression"] = true},
-    cards = {["j_seance"] = true, ["j_wrathful_joker"] = true, ["j_four_fingers"] = true, ["j_mime"] = true, ["j_raised_fist"] = true, ["j_pareidolia"] = true, ["j_dna"] = true, ["j_vampire"] = true, ["j_photograph"] = true, ["j_mr_bones"] = true, ["j_seeing_double"] = true, ["j_invisible"] = true, ["j_cbean_colon_mu_cube"] = true, ["j_cbean_colon_rna"] = true, ["j_cbean_colon_modernity"] = true, ["j_cbean_colon_spectaro"] = true, ["j_cbean_pboys_coriolis"] = true, ["j_cbean_nameteam_ghostimage"] = true, ["j_cbean_wgrop_withering_memory"] = true, ["j_hallucination"] = true, ["j_ceremonial"] = true, ["j_marble"] = true, ["j_scary_face"] = true, ["j_sixth_sense"] = true, ["j_faceless"] = true, ["j_idol"] = true, ["j_bloodstone"] = true, ["j_madness"] = true}
+    cards = {["j_seance"] = true, ["j_wrathful_joker"] = true, ["j_four_fingers"] = true, ["j_mime"] = true, ["j_raised_fist"] = true, ["j_pareidolia"] = true, ["j_dna"] = true, ["j_vampire"] = true, ["j_photograph"] = true, ["j_mr_bones"] = true, ["j_seeing_double"] = true, ["j_invisible"] = true, ["j_cbean_colon_mu_cube"] = true, ["j_cbean_colon_rna"] = true, ["j_cbean_colon_modernity"] = true, ["j_cbean_colon_spectaro"] = true, ["j_cbean_pboys_coriolis"] = true, ["j_cbean_nameteam_ghostimage"] = true, ["j_cbean_wgrop_withering_memory"] = true, ["j_hallucination"] = true, ["j_ceremonial"] = true, ["j_marble"] = true, ["j_scary_face"] = true, ["j_sixth_sense"] = true, ["j_faceless"] = true, ["j_idol"] = true, ["j_bloodstone"] = true, ["j_madness"] = true},
+    calculate = function(self, context)
+        if context.remove_playing_cards and not context.blueprint then
+            local cardsdestroyed = SMODS.shallow_copy(context.removed)
+            if #cardsdestroyed > 0 then
+                for i, v in ipairs(cardsdestroyed) do
+                    G.E_MANAGER:add_event(Event({
+                        trigger = 'after',
+                        delay = 0.3,
+                        func = function()
+                                local copy = copy_card(v)
+                                G.hand:emplace(copy)
+                                copy:set_ability('m_cbean_wgrop_pale_remnant')
+                                card_eval_status_text(copy, 'extra', nil, nil, nil, {message =('!'), colour = G.C.ATTENTION})
+                                table.insert(G.playing_cards, copy)
+                            return true
+                        end
+                    }))
+                end
+            end
+        end
+    end
 }
 
 
@@ -84,7 +111,10 @@ CBWG.ColdBeans_Biome {
     atlas = 'wgrop_biomes',
     key = "wgrop_volcanic",
     blinds = {["bl_needle"] = true, ["bl_flint"] = true,["bl_mouth"] = true,["bl_head"] = true, ["bl_final_heart"] = true, ["bl_cbean_colon_folly"] = true},
-    cards = {["j_rough_gem"] = true, ["j_golden"] = true, ["j_arrowhead"] = true, ["j_onyx_agate"] = true, ["j_madness"] = true, ["j_bloodstone"] = true, ["j_lusty"] = true, ["j_half"] = true, ["j_chaos"] = true, ["j_abstract"] = true, ["j_stone"] = true, ["j_glass"] = true, ["j_burnt"] = true, ["j_cbean_pboys_molotov"] = true, ["j_cbean_yma_charred_fool"] = true,}
+    cards = {["j_rough_gem"] = true, ["j_golden"] = true, ["j_arrowhead"] = true, ["j_onyx_agate"] = true, ["j_madness"] = true, ["j_bloodstone"] = true, ["j_lusty"] = true, ["j_half"] = true, ["j_chaos"] = true, ["j_abstract"] = true, ["j_stone"] = true, ["j_glass"] = true, ["j_burnt"] = true, ["j_cbean_pboys_molotov"] = true, ["j_cbean_yma_charred_fool"] = true,},
+    calculate = function(self, context)
+        
+    end
 }
 
 
@@ -95,6 +125,9 @@ CBWG.ColdBeans_Biome {
     key = "wgrop_glaciers",
     blinds = {["bl_arm"] = true, ["bl_fish"] = true, ["bl_water"] = true, ["bl_final_bell"] = true,["bl_cbean_colon_island"] = true, ["bl_cbean_colon_astro"] = true, ["bl_cbean_colon_sinker"] = true},
     cards = {["j_cbean_0chill_thorn_ring"] = true, ["j_mystic_summit"] = true, ["j_supernova"] = true, ["j_space"] = true, ["j_ice_cream"] = true, ["j_splash"] = true, ["j_blue_joker"] = true, ["j_constellation"] = true, ["j_cloud_9"] = true, ["j_rocket"] = true, ["j_castle"] = true, ["j_satellite"] = true, ["j_astronomer"] = true, ["j_cbean_pboys_yellow_snow"] = true, ["j_cbean_yma_seeing_stars"] = true, ["j_cbean_yma_well_wrapped"] = true, ["j_cbean_nameteam_hollyjollyjoker"] = true, ["j_cbean_nameteam_tipoftheiceberg"] = true, ["j_onyx_agate"] = true},
+    calculate = function(self, context)
+        
+    end
 }
 
 CBWG.ColdBeans_Biome {
@@ -104,6 +137,9 @@ CBWG.ColdBeans_Biome {
     key = "wgrop_city",
     blinds = {["bl_house"] = true, ["bl_wall"] = true, ["bl_cbean_colon_gate"] = true,  ["bl_final_bell"] = true, ["bl_cbean_colon_stamp"] = true, ["bl_cbean_colon_salesman"] = true, ["bl_cbean_colon_factory"] = true, ["bl_cbean_colon_pipeline"] = true, ["bl_cbean_colon_rebellion"] = true, ["bl_cbean_sdown_estrogen"] = true},
     cards = {["j_cbean_0chill_thorn_ring"] = true, ["j_seance"] = true, ["j_credit_card"] = true, ["j_loyalty_card"] = true, ["j_steel_joker"] = true, ["j_hack"] = true, ["j_scholar"] = true, ["j_business"] = true, ["j_ride_the_bus"] = true, ["j_burglar"] = true, ["j_blackboard"] = true, ["j_runner"] = true, ["j_card_sharp"] = true, ["j_red_card"] = true, ["j_shortcut"] = true, ["j_vagabond"] = true, ["j_luchador"] = true, ["j_gift"] = true, ["j_reserved_parking"] = true, ["j_baseball"] = true, ["j_diet_cola"] = true, ["j_trading"] = true, ["j_flash"] = true, ["j_ramen"] = true, ["j_walkie_talkie"] = true, ["j_certificate"] = true, ["j_throwback"] = true, ["j_blueprint"] = true, ["j_drivers_license"] = true, ["j_cbean_colon_chalk"] = true, ["j_cbean_colon_loan_shark"] = true, ["j_cbean_colon_sproinky"] = true, ["j_cbean_colon_big_shot"] = true, ["j_cbean_sdown_billboard"] = true, ["j_cbean_sdown_bisexual"] = true, ["j_cbean_0chill_lone_warrior"] = true, ["j_cbean_0chill_driving_in_my_truck"] = true, ["j_cbean_pboys_vodka"] = true, ["j_cbean_pboys_hydrant"] = true, ["j_cbean_pboys_flag_japan"] = true, ["j_cbean_pboys_restroom_closed"] = true, ["j_cbean_yma_born_salesman"] = true, ["j_cbean_yma_the_jest_around"] = true, ["j_cbean_nameteam_bettercreditcard"] = true, ["j_cbean_nameteam_alecwatson"] = true, ["j_cbean_nameteam_giveway"] = true, ["j_cbean_nameteam_presidenthathaway"] = true, ["j_cbean_nameteam_coffeemug"] = true, ["j_cbean_nameteam_giveway"] = true, ["j_cbean_nameteam_chuckmcgill"] = true,  },
+    calculate = function(self, context)
+        
+    end
 }
 -- ante 1 blinds have been removed as they get ignored and only the wall appears on ante 1
 -- all biomes should have a finisher blind or they get the wall
