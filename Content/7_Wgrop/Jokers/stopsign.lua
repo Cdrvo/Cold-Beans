553a7facb9aa284d855f073fb5e6b02f536c18a5
Tsutje <xundev90@gmail.com>
2025-12-25 19:43:54 +0800

    Add files via upload

--- Content/7_Wgrop/Jokers/stopsign.lua
+++ Content/7_Wgrop/Jokers/stopsign.lua
@@ -23,27 +23,64 @@ SMODS.Joker{
             }
         }
     end,
-
     calculate = function(self, card, context)
-        if context.after and G.GAME.current_round.hands_played == 0 and SMODS.calculate_round_score() then
-            if SMODS.calculate_round_score() > G.GAME.blind.chips then
-                card.ability.extra.percentage = math.abs(SMODS.calculate_round_score() - G.GAME.blind.chips) / ((SMODS.calculate_round_score() + G.GAME.blind.chips) / 2) * 100
-                return {
-                    message = "STOP!",
-                    colour = G.C.RED
-                }
-            else
+        if context.after then
+            G.GAME.wgrop_stopsign_blindwasskipped = false
+            card.ability.extra.percentage = 0
+            G.GAME.wgrop_stopsign_blindwillbeskipped = false
+            if G.GAME.current_round.hands_played == 0 and SMODS.calculate_round_score() then
+                if SMODS.calculate_round_score() > G.GAME.blind.chips then
+                    card.ability.extra.percentage = SMODS.calculate_round_score()/(G.GAME.blind.chips/100)
+                    return {
+                        message = "STOP!",
+                        colour = G.C.RED
+                    }
+                end
+                if card.ability.extra.percentage >= 500 then
+                    G.GAME.wgrop_stopsign_blindshopskipped = true
+                    G.GAME.wgrop_stopsign_blindwillbeskipped = true
+                elseif card.ability.extra.percentage >= 100 then
+                    G.GAME.wgrop_stopsign_blindwillbeskipped = true
+                    G.GAME.wgrop_stopsign_blindshopskipped = false
+                elseif card.ability.extra.percentage < 100 then
+                    G.GAME.wgrop_stopsign_blindwillbeskipped = false
+                    G.GAME.wgrop_stopsign_blindshopskipped = false
+                end
+            else 
                 card.ability.extra.percentage = 0
             end
         end
         if context.setting_blind and card.ability.extra.percentage ~= 0 then
             G.GAME.chips =  (G.GAME.blind.chips/100)*(card.ability.extra.percentage/2)
-            return {
-                message = "GO!",
-                colour = G.C.GREEN
-            }
+            if G.GAME.chips >= G.GAME.blind.chips then           
+                G.E_MANAGER:add_event(Event({
+                    trigger = 'after',
+                    delay = 0.3,
+                    func = function()
+                        end_round()
+                        G.E_MANAGER:add_event(Event({
+                            trigger = 'before',
+                            delay = 0.3,
+                            func = function()
+                                G.FUNCS.draw_from_hand_to_discard() 
+                                return true
+                            end
+                        }))
+                        return true
+                    end
+                }))
+                return {
+                    message = "GO!",
+                    colour = G.C.GREEN
+                }
+            end
+            card.ability.extra.percentage = 0
+        end
+        if context.starting_shop then
+            G.FUNCS.draw_from_hand_to_discard()    
         end
     end,
+
     beans_credits = {
     team = {"wgrop"
     },
