cd156c4599d9351cb4fb4c2de8faf03ec77a5250
Luna Astra Cassiopeia <kadabe380@gmail.com>
2025-12-25 19:44:22 +0800

    on the grapevine

--- Content/7_Wgrop/Enhancements/grapevine.lua
+++ Content/7_Wgrop/Enhancements/grapevine.lua
@@ -3,17 +3,29 @@ SMODS.Enhancement {
     key = 'wgrop_grapevine',
     pos = {x = 0, y = 5}, 
     atlas = 'wgrop_enhancement_atlas',
-    config = {},
+    config = {
+        extra = {
+            upgrade_mult = 2,
+        }
+    },
     loc_vars = function(self, info_queue, card)
         return { vars = {
+            card.ability.extra.upgrade_mult
         }}
     end,
     calculate = function (self, card, context)
+        if context.end_of_round and context.other_card == card and context.cardarea == G.hand then
+            context.other_card.ability.mult = (context.other_card.ability.mult or 0) + card.ability.extra.upgrade_mult
+            return {
+                message = localize('k_upgrade_ex'),
+                colour = G.C.MULT
+            }
+        end
     end,
     beans_credits = {
         team = {"wgrop"
         },
-        idea = "Tje.tsu",
+        idea = "Garb",
         art =  "gud_username",
         code = "Tje.tsu"
     }
