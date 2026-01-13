13cf1ea489b138cb09013e90eeb52eb7f72d86b4
MarioFan597 <81440403+MarioFan597@users.noreply.github.com>
2026-01-13 15:38:14 -0500

    Missing comma

--- Content/2_SeriousDownloading/items/jokers/path_in_the_woods.lua
+++ Content/2_SeriousDownloading/items/jokers/path_in_the_woods.lua
@@ -16,7 +16,7 @@ SMODS.Joker {
             "Played {C:attention}Queens{} lose their",
             "{C:attention}Enhancement{} and gain a",
             "random non-rankless {C:attention}Enhancement{}",
-            "{C:green}#1# in #2# chance{} to gain"
+            "{C:green}#1# in #2# chance{} to gain",
             "an {C:dark_edition}Edition{} instead"
         }
     },
