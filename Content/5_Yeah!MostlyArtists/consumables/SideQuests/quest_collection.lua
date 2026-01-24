265476e6e130ed0a890b01eb1dcb2e8f92709c7f
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2026-01-24 12:33:19 -0500

    Tboi Item Utils (2)
    Collection

--- Content/5_Yeah!MostlyArtists/consumables/SideQuests/quest_collection.lua
+++ Content/5_Yeah!MostlyArtists/consumables/SideQuests/quest_collection.lua
@@ -24,6 +24,15 @@ SMODS.current_mod.custom_collection_tabs = function()
         end
     end
 
+    local items = G.P_CENTER_POOLS["yma_tboi_items"]
+    local item_count = 0
+    local item_total = #side_quests
+    for _, item in ipairs(items) do
+        if item.discovered or (G.PROFILES[G.SETTINGS.profile] and G.PROFILES[G.SETTINGS.profile].all_unlocked) then
+            item_count = item_count + 1
+        end
+    end
+
     return {
         UIBox_button({
             button = 'your_collection_quests',
@@ -36,6 +45,18 @@ SMODS.current_mod.custom_collection_tabs = function()
             minh = 1,
             id = 'your_collection_quests',
             focus_args = { snap_to = true }
+        }),
+        UIBox_button({
+            button = 'your_collection_tboi_items',
+            label = { 'Items' },
+            item_count = item_total > 0 and {
+                tally = item_count,
+                of = item_total
+            } or nil,
+            minw = 5,
+            minh = 1,
+            id = 'your_collection_tboi_items',
+            focus_args = { snap_to = true }
         })
     }
 end
@@ -45,4 +66,22 @@ G.FUNCS.your_collection_quests = function()
     G.FUNCS.overlay_menu {
         definition = create_UIBox_quests(),
     }
+end
+
+G.FUNCS.your_collection_tboi_items = function()
+    G.SETTINGS.paused = true
+    G.FUNCS.overlay_menu {
+        definition = create_UIBox_tboi_items(),
+    }
+end
+
+function create_UIBox_tboi_items()
+
+    return SMODS.card_collection_UIBox(G.P_CENTER_POOLS["yma_tboi_items"], { 5, 6 }, {
+        snap_back = true,
+        hide_single_page = true,
+        collapse_single_page = true,
+        back_func = 'your_collection_other_gameobjects',
+        no_materialize = true,
+    })
 end
