0e3d4b98074580a76d73d00d88f55cc17bfc3ef3
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2026-01-24 14:16:33 -0500

    Cricket's Head

--- Content/5_Yeah!MostlyArtists/consumables/Items/tboi_items.lua
+++ Content/5_Yeah!MostlyArtists/consumables/Items/tboi_items.lua
@@ -120,56 +120,55 @@ YMA.TBOI_ITEMS {
 --        code = "RattlingSnow353",
 --    }
 --}
-----Cricket's Head
---YMA.TBOI_ITEMS {
---    key = "yma_tboi_crickets_head",
---    set = "yma_tboi_items",
---    order = 4,
---    quaility = 4,
---
---    loc_vars = function(self, info_queue, card)
---        return {
---            vars = {
---                card.ability.extra.xmult
---            }
---        }
---    end,
---
---    atlas = 'yma_tboi_atlas',
---    pos = { x = 0, y = 0 },
---    soul_pos = { x = 7, y = 2 },
---
---    config = {
---        extra = {
---            xmult = 1.5,
---            effect_table = {
---                ['x_mult'] = true,
---                ['xmult'] = true,
---                ['x_mult_mod'] = true,
---                ['Xmult'] = true,
---                ['Xmult_mod'] = true
---            }
---        }
---    },
---
---    calculate = function(self, card, context)
---        local temp_context = context.yma
---        if temp_context and temp_context.modify_card_effects and card.ability.extra.effect_table[temp_context.effect_type] then
---            print(temp_context.effects[temp_context.effect_type])
---            temp_context.effects[temp_context.effect_type] = ((temp_context.effects[temp_context.effect_type] - 1) * card.ability.extra.xmult) + 1
---            print(temp_context.effects[temp_context.effect_type])
---        end
---        if context.joker_main then
---            return {
---                xmult = card.ability.extra.xmult,
---                yma_avoid_modification = true,
---            }
---        end
---    end,
---    beans_credits = {
---        team = { "Yeah! Mostly Artists" },
---        idea = "Rainstar",
---        art = "RattlingSnow353",
---        code = "RattlingSnow353",
---    }
---}
+--Cricket's Head
+YMA.TBOI_ITEMS {
+    key = "yma_tboi_crickets_head",
+    set = "yma_tboi_items",
+    order = 4,
+    quaility = 4,
+
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {
+                card.ability.extra.xmult
+            }
+        }
+    end,
+
+    atlas = 'yma_tboi_atlas',
+    pos = { x = 0, y = 0 },
+    soul_pos = { x = 7, y = 2 },
+
+    config = {
+        extra = {
+            xmult = 1.5,
+            effect_table = {
+                ['x_mult'] = true,
+                ['xmult'] = true,
+                ['x_mult_mod'] = true,
+                ['Xmult'] = true,
+                ['Xmult_mod'] = true
+            }
+        }
+    },
+
+    calculate = function(self, card, context)
+        local temp_context = context.yma
+        if temp_context and temp_context.modify_card_effects and card.ability.extra.effect_table[temp_context.effect_type] then
+            temp_context.effects[temp_context.effect_type] = ((temp_context.effects[temp_context.effect_type] - 1) * card.ability.extra.xmult) + 1
+            temp_context.effects['message'] = localize{type='variable',key='a_xmult',vars={temp_context.effects[temp_context.effect_type]}} --Potentally problomatic, Fixes visual junk with vanilla Jokers
+        end
+        if context.joker_main then
+            return {
+                xmult = card.ability.extra.xmult,
+                yma_avoid_modification = true,
+            }
+        end
+    end,
+    beans_credits = {
+        team = { "Yeah! Mostly Artists" },
+        idea = "Rainstar",
+        art = "RattlingSnow353",
+        code = "RattlingSnow353",
+    }
+}
