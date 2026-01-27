f0f74085190d58423ecfe2d7e079e13a1d3109f8
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2026-01-27 01:24:54 -0500

    Uncommon Medal and Rare Medal

--- Content/5_Yeah!MostlyArtists/consumables/Medals/medals.lua
+++ Content/5_Yeah!MostlyArtists/consumables/Medals/medals.lua
@@ -43,3 +43,115 @@ YMA.MEDAL {
         code = "RattlingSnow353",
     }
 }
+--Unommon Medal
+YMA.MEDAL {
+    key = "yma_uncommon",
+    order = 2,
+    badge_colour = HEX("4bc292"),
+
+    loc_vars = function(self, info_queue, card)
+        local numerator, denominator = SMODS.get_probability_vars(card, 1, card.ability.yma_medal.extra.odds, 'yma_uncommon')
+        return {
+            vars = {
+                card.ability.yma_medal.extra.chips,
+                card.ability.yma_medal.extra.mult,
+                numerator, denominator,
+            }
+        }
+    end,
+
+    atlas = 'yma_medals_atlas',
+    pos = { x = 3, y = 0 },
+
+    config = {
+        extra = {
+            chips = 50,
+            mult = 10,
+            odds = 4,
+        }
+    },
+
+    calculate = function(self, card, context)
+        if context.joker_main then
+            local which = pseudorandom("yma_uncommon_pick", 1, 3)
+            local chips = (which == 1 and card.ability.yma_medal.extra.chips or nil)
+            local mult = (which == 2 and card.ability.yma_medal.extra.mult or nil)
+            local dollars = (which == 3 and SMODS.pseudorandom_probability(card, 'yma_uncommon', 1, card.ability.yma_medal.extra.odds) and 5 or nil)
+            return {
+                chips = chips,
+                mult = mult,
+                dollars = dollars,
+            }
+        end
+    end,
+    beans_credits = {
+        team = { "Yeah! Mostly Artists" },
+        idea = "RattlingSnow353",
+        art = "RattlingSnow353",
+        code = "RattlingSnow353",
+    }
+}
+--Rare Medal
+YMA.MEDAL {
+    key = "yma_rare",
+    order = 3,
+    badge_colour = HEX("fe5f55"),
+
+    loc_vars = function(self, info_queue, card)
+        local numerator, denominator = SMODS.get_probability_vars(card, 1, card.ability.yma_medal.extra.odds, 'yma_rare')
+        return {
+            vars = {
+                card.ability.yma_medal.extra.xchips,
+                card.ability.yma_medal.extra.xmult,
+                numerator, denominator,
+            }
+        }
+    end,
+
+    atlas = 'yma_medals_atlas',
+    pos = { x = 0, y = 0 },
+
+    config = {
+        extra = {
+            xchips = 1.5,
+            xmult = 1.2,
+            odds = 4,
+        }
+    },
+
+    calculate = function(self, card, context)
+        if context.joker_main then
+            local which = pseudorandom("yma_rare_pick", 1, 3)
+            local xchips = (which == 1 and card.ability.yma_medal.extra.xchips or nil)
+            local xmult = (which == 2 and card.ability.yma_medal.extra.xmult or nil)
+            if which == 3 then
+                if SMODS.pseudorandom_probability(card, 'yma_rare', 1, card.ability.yma_medal.extra.odds) and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
+                    G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
+                    G.E_MANAGER:add_event(Event({
+                        trigger = 'before',
+                        delay = 0.0,
+                        func = (function()
+                            local cardd = create_card('Consumeables',G.consumeables, nil, nil, nil, nil, nil, 'yma_rare_card')
+                            cardd:add_to_deck()
+                            G.consumeables:emplace(cardd)
+                            G.GAME.consumeable_buffer = 0
+                            return true
+                        end)
+                    }))
+                end
+            else
+                return {
+                    xchips = xchips,
+                    xmult = xmult,
+                }
+            end
+            
+        end
+    end,
+    beans_credits = {
+        team = { "Yeah! Mostly Artists" },
+        idea = "RattlingSnow353",
+        art = "RattlingSnow353",
+        code = "RattlingSnow353",
+    }
+}
