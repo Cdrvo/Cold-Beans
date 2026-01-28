f0e22402e8334aff7a380bb52f0d9b83233d1cc4
CodeRevo <devrimulasyurtsever00@gmail.com>
2026-01-28 06:01:42 +0300

    Starfruit

--- Content/6_NameTeam/Plants/rare_plants.lua
+++ Content/6_NameTeam/Plants/rare_plants.lua
@@ -52,3 +52,39 @@ SMODS.Joker({
         end
     end,
 })
+
+SMODS.Joker({
+    key = "starfruit",
+    cost = 5,
+    rarity = 3,
+    blueprint_compat = true,
+    config = {
+        extra = {
+            mult = 10,
+            chips = 15,
+            xmult = 1.5,
+            dollars = 1,
+            discards = 1
+        }
+    },
+    loc_vars = function(self,info_queue,card)
+        local cae = card.ability.extra
+        return{vars={cae.chips,cae.mult,cae.xmult,cae.dollars,cae.discards}}
+    end,
+    calculate = function(self,card,context)
+        local cae = card.ability.extra
+        if context.setting_blind then
+            ease_discard(cae.discards)
+        end
+        if context.joker_main then
+            return{
+                xmult = cae.xmult,
+                chips = cae.chips,
+                mult = cae.mult
+            }
+        end
+    end,
+    calc_dollar_bonus = function(self,card)
+        return card.ability.extra.dollars
+    end
+})
