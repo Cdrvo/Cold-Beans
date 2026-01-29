3a196ec21d94cfcbd38854e1c7d565fadd6f7c62
Alvin Dong <alvindong9913@gmail.com>
2026-01-28 22:03:26 -0600

    fix sprite pos

--- Content/6_NameTeam/StickerSheets.lua
+++ Content/6_NameTeam/StickerSheets.lua
@@ -1266,7 +1266,7 @@ SMODS.Consumable({
 	set = "cbean_StickerSheet",
 	key = "barren_sheet",
 	pos = { x = 0, y = 0 },
-	pos_extra = { x = 1, y = 5 },
+	pos_extra = { x = 3, y = 6 },
 	draw_extra = function(self, card, layer)
 		if self.discovered or card.params.bypass_discovery_center then
 			card.cbean_extra:draw_shader("booster", nil, card.ARGS.send_to_shader, nil, card.children.center)
