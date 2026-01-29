fd63e7912a5ac33e933d43893d18bbc62723002d
Alvin Dong <alvindong9913@gmail.com>
2026-01-28 22:01:08 -0600

    nil check

--- Content/6_NameTeam/Stickers/NegativeStickers/spore_sticker.lua
+++ Content/6_NameTeam/Stickers/NegativeStickers/spore_sticker.lua
@@ -29,7 +29,7 @@ SMODS.Sticker({
 			end
             local _card = NAMETEAM.random_joker(card.area.cards, card)
             local ae = NAMETEAM.poll_sticker(true, _card, false, _set)
-			if ae then
+			if ae and _card then
 				_card:add_sticker(ae, true)
 			end
 		end
