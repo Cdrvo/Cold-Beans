8c59cf2011b38b5f2dd10bd0f27e33d0d590f51b
Alvin Dong <alvindong9913@gmail.com>
2026-01-29 21:49:23 -0600

    fix shield jank

--- Content/6_NameTeam/Stickers/PositiveStickers/shield_sticker.lua
+++ Content/6_NameTeam/Stickers/PositiveStickers/shield_sticker.lua
@@ -19,22 +19,22 @@ SMODS.Sticker({
 	rate = 0.03,
 	sticker_type = "Positive",
 	calculate = function(self, card, context)
-		if context.cbean_destroyed and context.cbean_destroyed_card == card then
-            local area
+		-- if context.cbean_destroyed and context.cbean_destroyed_card == card then
+        --     local area
 
-            if context.cbean_destroyed and context.cbean_destroyed_card.area then
-				area = context.cbean_destroyed_card.area
-			end
+        --     if context.cbean_destroyed and context.cbean_destroyed_card.area then
+		-- 		area = context.cbean_destroyed_card.area
+		-- 	end
 
-			local ccard = copy_card(card)
-			ccard:add_to_deck()
-			area:emplace(ccard)
+		-- 	local ccard = copy_card(card)
+		-- 	ccard:add_to_deck()
+		-- 	area:emplace(ccard)
 
-			play_sound("cbean_shielddefend", 1, 0.5)
-			SMODS.calculate_effect({ message = "Shielded!"}, ccard)
+		-- 	play_sound("cbean_shielddefend", 1, 0.5)
+		-- 	SMODS.calculate_effect({ message = "Shielded!"}, ccard)
 
-            SMODS.Stickers["cbean_shield"]:apply(ccard, false)
-		end
+        --     SMODS.Stickers["cbean_shield"]:apply(ccard, false)
+		-- end
 	end,
 	beans_credits = {
         code = "Revo",
