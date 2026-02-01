3386883c7c9183fafa9d5b5470ec14e30af903c9
Alvin Dong <alvindong9913@gmail.com>
2026-02-01 13:48:17 -0600

    Implement true home

--- Content/2_SeriousDownloading/items/handtypes/house_rules_hands/true_home.lua
+++ Content/2_SeriousDownloading/items/handtypes/house_rules_hands/true_home.lua
@@ -1,41 +1,92 @@
-SMODS.PokerHand{
-    key = "sdown_true_home",
-    l_chips = 50,
-    l_mult = 4,
-    chips = 145,
-    mult = 15,
-    cb_house_rules = true,
-    visible = function (self)
-        local joker = not not next(SMODS.find_card('j_cbean_0chill_house_rules'))
-        local clicked
-        if G.jokers and G.jokers.highlighted then
-            for _,v in ipairs(G.jokers.highlighted) do
-                if v.config.center.key == "j_cbean_0chill_house_rules" then clicked = true end
-            end
-        end
-        local played = G.GAME.hands and G.GAME.hands[self.key] and (G.GAME.hands[self.key].played or 0) > 0
+SMODS.PokerHand({
+	key = "sdown_true_home",
+	l_chips = 50,
+	l_mult = 4,
+	chips = 145,
+	mult = 15,
+	cb_house_rules = true,
+	visible = function(self)
+		local joker = not not next(SMODS.find_card("j_cbean_0chill_house_rules"))
+		local clicked
+		if G.jokers and G.jokers.highlighted then
+			for _, v in ipairs(G.jokers.highlighted) do
+				if v.config.center.key == "j_cbean_0chill_house_rules" then
+					clicked = true
+				end
+			end
+		end
+		local played = G.GAME.hands and G.GAME.hands[self.key] and (G.GAME.hands[self.key].played or 0) > 0
 
-        return joker and (played or clicked)
-    end,
-    example = {
-        { "S_A", true},
-        { "S_A", true},
-        { "D_K", true},
-        { "D_K", true},
-        { "D_K", true},
-    },
-    evaluate = function(parts, hand)
-        if (#SMODS.find_card('j_cbean_0chill_house_rules') > 0) and ((#SMODS.find_card('j_seeing_double') > 0) or (#SMODS.find_card('j_cbean_nameteam_diamondshapewithadotinside') > 0)) then --Checks if house rules and required joker are owned
-            if not next(parts._straight) then return {} end
-            local stones = {}
-            for _, card in ipairs(hand) do
-                if  (SMODS.has_enhancement(card, 'm_stone')) then
-                    table.insert(stones,card)
-                end
-            end
-            if #stones >= 5 then
-                return {stones}
-            else return {} end
-        end
-    end
-}
+		return joker and (played or clicked)
+	end,
+	example = {
+		{ "S_A", true },
+		{ "S_A", true },
+		{ "D_K", true },
+		{ "D_K", true },
+		{ "D_K", true },
+	},
+	evaluate = function(parts, hand)
+		if
+			(#SMODS.find_card("j_cbean_0chill_house_rules") > 0)
+			and (
+				(#SMODS.find_card("j_seeing_double") > 0)
+				or (#SMODS.find_card("j_cbean_nameteam_diamondshapewithadotinside") > 0)
+			)
+		then --Checks if house rules and required joker are owned
+			if #parts._3 < 1 or #parts._2 < 2 then
+				return {}
+			end
+			local unique_suits = {}
+			local threes = {}
+			local twos = {}
+            for _, cards in ipairs(parts._3) do
+				for suit, _ in pairs(SMODS.Suits) do
+					local t = {}
+					local flush_count = 0
+					for _, c in pairs(cards) do
+						if c:is_suit(suit, nil, true) then
+							t[#t + 1] = c
+							flush_count = flush_count + 1
+						end
+					end
+					if flush_count >= 3 then
+						threes[#threes + 1] = t
+					end
+				end
+			end
+			for _, cards in ipairs(parts._2) do
+				for suit, _ in pairs(SMODS.Suits) do
+					local t = {}
+					local flush_count = 0
+					for _, c in pairs(cards) do
+						local in_three = false
+						for _, threes in ipairs(parts._3) do
+							for _, c2 in pairs(threes) do
+								if c == c2 then
+									in_three = true
+									break
+								end
+							end
+							if in_three then
+								break
+							end
+						end
+						if c:is_suit(suit, nil, true) and not in_three then
+							t[#t + 1] = c
+							flush_count = flush_count + 1
+						end
+					end
+					if flush_count >= 2 then
+						twos[#twos + 1] = t
+					end
+				end
+			end
+			
+			if #twos < 1 or #threes < 1 then
+				return {}
+			end
+			return { SMODS.merge_lists(twos, threes) }
+		end
+	end,
+})
