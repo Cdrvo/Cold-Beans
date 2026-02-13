74e536723dbe0365e845804fd653b6efc4b207bc
kierkat10 <kiernan.simpson@gmail.com>
2026-02-12 23:32:15 -0700

    change inexcusable to ban ceo tag instead of boss

--- Content/2_SeriousDownloading/items/challenges/inexcusable.lua
+++ Content/2_SeriousDownloading/items/challenges/inexcusable.lua
@@ -2,15 +2,15 @@ SMODS.Challenge {
     key = 'sdown_inexcusable',
     rules = {
         custom = {
-            {id = 'cbean_sdown_all_blinds_are', value = "bl_cbean_sdown_folly"},
+            { id = 'cbean_sdown_all_blinds_are', value = "bl_cbean_sdown_folly" },
         }
     },
-    restrictions = {banned_cards = {
-        {id = "j_chicot"},
-        {id = "j_luchador"},
-        {id = "v_directors_cut"},
-        {id = "v_retcon"},
+    restrictions = { banned_cards = {
+        { id = "j_chicot" },
+        { id = "j_luchador" },
+        { id = "v_directors_cut" },
+        { id = "v_retcon" },
     }, banned_tags = {
-        {id = "tag_boss"}
+        { id = "tag_cbean_colon_ceoreroll" }
     }}
 }
