3e2846dc4d58f0d6eceaad578ec1b988ed3630fb
MarioFan597 <81440403+MarioFan597@users.noreply.github.com>
2026-02-14 22:02:56 -0500

    Actually made softlock work properly

--- Content/2_SeriousDownloading/items/editions/frozen.lua
+++ Content/2_SeriousDownloading/items/editions/frozen.lua
@@ -1,7 +1,21 @@
-
 local click = Card.click
 function Card:click()
-    if self.edition and self.edition.type == "cbean_sd_frozen" and not ((#SMODS.find_card('j_cbean_0chill_noelle') > 0) and (#SMODS.find_card('j_cbean_0chill_thorn_ring') > 0)) and (not allfrozencards and #G.hand.highlighted > 0) then
+    local allfrozencards = false --Checks if all cards are Frozen. Needed to prevent softlocks
+    local frozen_count = 0
+    for k, v in pairs(G.hand.cards) do
+        if v.edition and v.edition.cbean_sd_frozen then
+            frozen_count = frozen_count + 1
+        end
+    end
+    print(#G.hand.highlighted)
+    if frozen_count == #G.hand.cards then
+        allfrozencards = true
+        print("True")
+    else
+        allfrozencards = false
+    end
+
+    if self.edition and self.edition.type == "cbean_sd_frozen" and not ((#SMODS.find_card('j_cbean_0chill_noelle') > 0) and (#SMODS.find_card('j_cbean_0chill_thorn_ring') > 0)) and not (allfrozencards and #G.hand.highlighted == 0) then
         if self.area and self.area == G.deck and self.area.cards[1] == self then 
             G.FUNCS.deck_info()
         end
@@ -57,20 +71,3 @@ SMODS.Edition{
         code = "mys. minty",
     }
 }
-
-local allfrozencards = false --Checks
-local frozen_count = 0
-SMODS.current_mod.calculate = function(self,context)
-    for k, v in pairs(G.hand.cards) do
-        if v.edition and v.edition.cbean_sd_frozen then
-            frozen_count = frozen_count + 1
-        end
-    end
-    print(frozen_count)
-    if frozen_count == #G.hand.cards then
-        allfrozencards = true
-    else
-        allfrozencards = false
-    end
-    frozen_count = 0
-end
