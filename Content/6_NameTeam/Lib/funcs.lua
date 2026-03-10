5859c0a2d49a2dad33d8f8b26a5fd88b1115dc1d
= <devrimulasyurtsever00@gmail.com>
2026-03-10 16:00:05 +0300

    fix all upgraded blackholes

--- Content/6_NameTeam/Lib/funcs.lua
+++ Content/6_NameTeam/Lib/funcs.lua
@@ -884,7 +884,8 @@ function NAMETEAM.shop_sign(sign)
 	}))
 end
 
-function NAMETEAM.blackhole(from, allow_sound)
+function NAMETEAM.blackhole(from, allow_sound, by)
+	by = by or 1
 	update_hand_text({sound = 'button', volume = 0.7, pitch = 0.8, delay = 0.3}, {handname=localize('k_all_hands'),chips = '...', mult = '...', level=''})
         G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.2, func = function()
 			if allow_sound then
@@ -908,10 +909,10 @@ function NAMETEAM.blackhole(from, allow_sound)
             from:juice_up(0.8, 0.5)
             G.TAROT_INTERRUPT_PULSE = nil
             return true end }))
-        update_hand_text({sound = 'button', volume = 0.7, pitch = 0.9, delay = 0}, {level='+1'})
+        update_hand_text({sound = 'button', volume = 0.7, pitch = 0.9, delay = 0}, {level='+' .. by})
         delay(1.3)
         for k, v in pairs(G.GAME.hands) do
-            level_up_hand(from, k, true)
+            level_up_hand(from, k, true, by)
         end
         update_hand_text({sound = 'button', volume = 0.7, pitch = 1.1, delay = 0}, {mult = 0, chips = 0, handname = '', level = ''})
 end
